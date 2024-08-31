dfa3959633162ee471f4ce61ec38e88f52ffc6bc
Raynor119 <deivicamiloflorezprieto@gmail.com>
2024-08-31 15:00:06 -0500

    calculo de la merma

--- app/src/main/java/com/example/orobackoup/MainActivity.java
+++ app/src/main/java/com/example/orobackoup/MainActivity.java
@@ -6,9 +6,12 @@ import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -20,18 +23,25 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.material.textfield.TextInputEditText;
+
 public class MainActivity extends AppCompatActivity {
     private static final int REQUEST_IMAGE_CAPTURE = 1;
     private static final int PERMISSIONS_REQUEST_CAMERA = 2;
     CardView btnCamara;
     CardView btnCamarav2;
     ImageView imgView;
+    TextInputEditText PesoInicial,pesofinal;
+    TextView mermatext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         btnCamara = findViewById(R.id.foto);
+        PesoInicial= findViewById(R.id.PesoInicial);
+        pesofinal= findViewById(R.id.pesofinal);
+        mermatext=findViewById(R.id.merma);
         btnCamarav2 = findViewById(R.id.fotoRealizada);
         imgView = findViewById(R.id.fotoprenda);
         btnCamara.setOnClickListener(new View.OnClickListener() {
@@ -64,6 +74,21 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+        TextWatcher watcher = new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) { }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                calcularMerma();
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) { }
+        };
+
+        PesoInicial.addTextChangedListener(watcher);
+        pesofinal.addTextChangedListener(watcher);
 
     }
     private void abrirCamara(){
@@ -80,7 +105,25 @@ public class MainActivity extends AppCompatActivity {
             Bitmap imgBitmap = (Bitmap) extras.get("data");
             imgView.setImageBitmap(imgBitmap);
             btnCamara.setVisibility(View.GONE);
-            Toast.makeText(this, "se tomo la foto", Toast.LENGTH_LONG).show();
+            //Toast.makeText(this, "se tomo la foto", Toast.LENGTH_LONG).show();
+        }
+    }
+    private void calcularMerma() {
+        String pesoInicialStr = PesoInicial.getText().toString();
+        String pesoFinalStr = pesofinal.getText().toString();
+
+        if (!pesoInicialStr.isEmpty() && !pesoFinalStr.isEmpty()) {
+            try {
+                double pesoInicial = Double.parseDouble(pesoInicialStr);
+                double pesoFinal = Double.parseDouble(pesoFinalStr);
+                double merma = pesoInicial - pesoFinal;
+                mermatext.setText(String.format("La merma es: %.2f", merma));
+            } catch (NumberFormatException e) {
+                // Manejo de errores si el formato del número es incorrecto
+                mermatext.setText("La merma es:: 0");
+            }
+        } else {
+            mermatext.setText("La merma es:a: 0");
         }
     }
     @Override
