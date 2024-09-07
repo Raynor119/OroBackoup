ec77fcec7d8b5426d381f5f3568be5e5997fbee9
Raynor119 <deivicamiloflorezprieto@gmail.com>
2024-09-07 17:13:28 -0500

    Inicio de session iniciado

---
+++ app/src/main/java/com/pixels/orobackoup/View/InicioSesion/InicioSession.java
@@ -0,0 +1,19 @@
+package com.pixels.orobackoup.View.InicioSesion;
+
+import android.os.Bundle;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import com.pixels.orobackoup.R;
+
+public class InicioSession extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inicio_session);
+    }
+}
