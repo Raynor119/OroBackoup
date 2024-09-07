5fbf491e6efbd5ef923c60105f7b1a5855742eda
Raynor119 <deivicamiloflorezprieto@gmail.com>
2024-09-07 17:27:11 -0500

    update upgrade

---
+++ app/src/main/java/com/pixels/orobackoup/View/Menu/Menu.java
@@ -0,0 +1,20 @@
+package com.pixels.orobackoup.View.Menu;
+
+import android.os.Bundle;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.pixels.orobackoup.R;
+
+public class Menu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+    }
+}
