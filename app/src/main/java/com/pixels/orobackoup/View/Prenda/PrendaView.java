8f06e47b052a34232d28a11f3a6a1279c9faeffe
Raynor119 <deivicamiloflorezprieto@gmail.com>
2024-09-17 08:31:17 -0500

    copia para windowws

---
+++ app/src/main/java/com/pixels/orobackoup/View/Prenda/PrendaView.java
@@ -0,0 +1,21 @@
+package com.pixels.orobackoup.View.Prenda;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.pixels.orobackoup.R;
+
+public class PrendaView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_prenda_view);
+
+    }
+}
