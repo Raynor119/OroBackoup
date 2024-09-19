0ac8127f8ec6cb9e81f924b11a8f132abcf6acb5
Raynor119 <deivicamiloflorezprieto@gmail.com>
2024-09-19 13:52:38 -0500

    codigo de prenda generado

--- app/src/main/java/com/pixels/orobackoup/Model/BD/MYSQL/Consultas/Menu/AgregarPrendaMYSQL.java
+++ app/src/main/java/com/pixels/orobackoup/Model/BD/MYSQL/Consultas/Menu/AgregarPrendaMYSQL.java
@@ -4,16 +4,19 @@ import android.content.Context;
 import android.widget.Toast;
 
 import com.mysql.jdbc.PreparedStatement;
+import com.mysql.jdbc.Statement;
 import com.pixels.orobackoup.Model.BD.MYSQL.Conexion;
 import com.pixels.orobackoup.ViewModel.Menu.AgregarPrendaViewModel;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 
 public class AgregarPrendaMYSQL extends Conexion {
     private boolean verficar=true;
     private boolean verificarE=false;
     private String CodigoU,NombreP;
+    private int CodigoP=0;
     private AgregarPrendaViewModel ViewModel;
     public AgregarPrendaMYSQL(Context context,String codigoU,String nombreP,AgregarPrendaViewModel viewModel) {
         super(context);
@@ -42,14 +45,19 @@ public class AgregarPrendaMYSQL extends Conexion {
                 return "Error en la conexion";
             }else{
                 String Sql="INSERT INTO Prendas (codigousu, nombre) VALUES (?, ?)";
-                PreparedStatement stmt = (PreparedStatement) connection.prepareStatement(Sql);
+                PreparedStatement stmt = (PreparedStatement) connection.prepareStatement(Sql, Statement.RETURN_GENERATED_KEYS);
                 stmt.setInt(1, Integer.parseInt(CodigoU));
                 stmt.setString(2, NombreP);
                 stmt.executeUpdate();
+                ResultSet generatedKeys = stmt.getGeneratedKeys();
+                if (generatedKeys.next()) {
+                    int codigoGenerado = generatedKeys.getInt(1); // El código generado
+                    CodigoP=codigoGenerado;
+                }
                 return "";
             }
         }catch (Exception e){
-            return "Error al guardar los datos del producto en la Base de Datos";
+            return "Error al guardar los datos del producto en la Base de Datos e:"+e;
         }
     }
     @Override
@@ -57,6 +65,7 @@ public class AgregarPrendaMYSQL extends Conexion {
         verificarE=true;
         if(result.equals("")){
             verficar=true;
+            //Toast.makeText(Context, "codigo generado:"+CodigoP, Toast.LENGTH_SHORT).show();
             ConsultaBaseDatos();
         }else {
             verficar=false;
