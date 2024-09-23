6786af6e4850571df13bf3c8b8d0c17a12d13728
Raynor119 <deivicamiloflorezprieto@gmail.com>
2024-09-23 12:58:17 -0500

    lista de prendasv1

--- app/src/main/java/com/pixels/orobackoup/View/Prenda/ListaPrendas/PrendasListView.java
+++ app/src/main/java/com/pixels/orobackoup/View/Prenda/ListaPrendas/PrendasListView.java
@@ -1,19 +1,58 @@
 package com.pixels.orobackoup.View.Prenda.ListaPrendas;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
+import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.pixels.orobackoup.R;
 
 public class PrendasListView extends AppCompatActivity {
 
+    private SharedPreferences prefe;
+    private String CodigoU,TipoU;
+    public RecyclerView recyclerView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_prendas_list_view);
+        prefe=getSharedPreferences("Sesion",PrendasListView.this.MODE_PRIVATE);
+        recyclerView = (RecyclerView) findViewById(R.id.opcion_list);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        TipoU=prefe.getString("TipoUsuario","0");
+        if(TipoU.equals("A")){
+            CodigoU="none";
+        }else{
+            CodigoU=prefe.getString("Codigo","0");
+        }
+        Toolbar toolbar = (Toolbar) findViewById(R.id.detail_toolbar);
+        setTitle("");
+        setSupportActionBar(toolbar);
+        reclicler();
+    }
+    @Override
+    public boolean onCreateOptionsMenu(android.view.Menu menu){
+        getMenuInflater().inflate(R.menu.menuupdate,menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        int id= item.getItemId();
+        if(id == R.id.opcion){
+            reclicler();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void reclicler(){
+
     }
 }
--- app/src/main/java/com/pixels/orobackoup/View/Prenda/ListaPrendas/RecyclerViewAdapter/RecyclerAdaptadorPrendas.java
+++ app/src/main/java/com/pixels/orobackoup/View/Prenda/ListaPrendas/RecyclerViewAdapter/RecyclerAdaptadorPrendas.java
@@ -1,4 +1,64 @@
 package com.pixels.orobackoup.View.Prenda.ListaPrendas.RecyclerViewAdapter;
 
-public class RecyclerAdaptadorPrendas {
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.pixels.orobackoup.Model.DatosEncapsulados.ListaPrenda;
+import com.pixels.orobackoup.R;
+import com.pixels.orobackoup.View.Prenda.ListaPrendas.PrendasListView;
+
+import java.util.List;
+
+public class RecyclerAdaptadorPrendas extends RecyclerView.Adapter<RecyclerAdaptadorPrendas.ViewHolder> {
+    private final PrendasListView MParentActivity;
+    private final List<ListaPrenda> MValues;
+
+    public RecyclerAdaptadorPrendas(PrendasListView ParentActivity,List<ListaPrenda> mValues){
+        this.MParentActivity=ParentActivity;
+        this.MValues=mValues;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.lista_prendas, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        holder.imagen.setImageResource(R.drawable.baseline_dashboard_24);
+        holder.mContentView.setText(MValues.get(position).getNombre());
+        holder.fechaR.setText(MValues.get(position).getFecha());
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return MValues.size();
+    }
+
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        public final TextView mContentView,fechaR;
+        public final ImageView imagen;
+
+        public ViewHolder(View view) {
+            super(view);
+            imagen=(ImageView) view.findViewById(R.id.imagen);
+            mContentView = (TextView) view.findViewById(R.id.content);
+            fechaR = (TextView) view.findViewById(R.id.fecha);
+        }
+    }
 }
